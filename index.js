b6a9825690433df850c8bd2461369a40f9afbfb0
Inês Almeida <ines_almeida_203@hotmail.com>
2023-05-06 14:54:35 +0100

    add images

--- index.js
+++ index.js
@@ -26,9 +26,13 @@ const myGameArea = {
 const beachBackground = new Image();
 beachBackground.src = './images/game_background_3.png';
 
+const ingredientsList = new Image
+ingredientsList.src = './images/display ingredients.png'
+
 function drawBackground() {
   const ctx = myGameArea.context;
   ctx.drawImage(beachBackground, 0, 0, 1450, 700);
+  ctx.drawImage(ingredientsList, 1200, 10, 150, 150);
 }
 
 class Person {
@@ -122,7 +126,7 @@ const glass6 = new Image()
 glass6.src = './images/glass6.png'
 
 function drawGlass(ctx) {
-    ctx.drawImage(glass, 1300, 10, 100, 150);
+    ctx.drawImage(glass, 1350, 10, 75, 150);
 //draw glass when player touch the correct ingredients
     /*if(plays touch CorrectIngredient draw glass)
     ctx.drawImage(glass1, 1300, 10, 100, 150);
@@ -135,6 +139,7 @@ function drawGlass(ctx) {
   } 
 
 
+
 function updateGameArea() {
   myGameArea.clear();
   drawBackground();
@@ -144,6 +149,7 @@ function updateGameArea() {
   vodka.update();
   tabasco.update();
   drawGlass(myGameArea.context);
+  i
 }
 
 myGameArea.start();
